eb5b9856e358d74641b46403fb6e71953961e8f8
Sangram Pattnaik <sangrampattnaik09@gmail.com>
2022-02-28 22:41:19 +0530

    pdated

--- config/deploy.sh
+++ config/deploy.sh
@@ -2,6 +2,7 @@
 set -e
 echo "deploying..."
 directory='../'
+source $directory/venv/bin/actiate
 command="$directory/venv/bin/python"
 $command $directory/manage.py collectstatic --noinput
 cp -r $directory/config/nginx.conf .
