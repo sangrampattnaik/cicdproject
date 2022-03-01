2e36d6ad1b19d07aaa28e004adf3f90afb803629
Sangram Pattnaik <sangrampattnaik09@gmail.com>
2022-03-01 09:29:07 +0530

    updated

--- testapp/views.py
+++ testapp/views.py
@@ -2,6 +2,6 @@ from django.http import JsonResponse
 
 
 def home(request):
-    return JsonResponse({"status": "success"})
+    return JsonResponse({"status": "success", "message": "home page"})
 
 
